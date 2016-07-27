a489a19c221cfac31d55345627b7052c3deef349
Joseph Hindin <hindin@nkloom.com>
2016-07-27 10:40:02 +0300

    Initial commit, with intentional typos

---
+++ aaa.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This program has been cmpiled from aaa.c\n");
+}
+
---
+++ bbb.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This program has been compild from bbb.c\n");
+}
+
